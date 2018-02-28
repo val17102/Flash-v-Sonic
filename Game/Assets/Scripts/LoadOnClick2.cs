a2bad93487f55c35d848c9454c9eaf2b3a86fc22
val17102 <val17102@uvg.edu.gt>
2018-02-27 23:30:39 -0600

    5
    5

---
+++ Game/Assets/Scripts/LoadOnClick2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick2: MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneName:"Map 2");
+    }
+}
