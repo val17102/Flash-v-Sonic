6caef3fb5da1ddd173082da7940dcd88748fbf5e
Mac17369 <Mac17369@uvg.edu.gt>
2018-02-27 18:51:02 -0600

    Actualizacion

--- Game/Assets/Scripts/Move.cs
+++ Game/Assets/Scripts/Move.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Move : MonoBehaviour {
     public KeyCode upKey;
@@ -13,6 +14,7 @@ public class Move : MonoBehaviour {
     public float speed = 16;
     Animator anim;
     SpriteRenderer sr;
+
     void spawnWall()
     {
         lastWallEnd = transform.position;
